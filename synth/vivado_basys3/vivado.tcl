e02ea5b780170a84a2da14883384cb53fafcd528
Aydan Olaez <aolaez@ucsc.edu>
2025-02-06 15:32:13 -0800

    blinky on board, working on audio

--- synth/vivado_basys3/vivado.tcl
+++ synth/vivado_basys3/vivado.tcl
@@ -1,4 +1,3 @@
-
 # start_gui
 
 create_project basys3 basys3 -part xc7a35tcpg236-1
@@ -8,6 +7,9 @@ create_project basys3 basys3 -part xc7a35tcpg236-1
 
 add_files -norecurse {
  ../basys3.sv
+ ../../../rtl/blinky.sv
+ ../../../rtl/config_pkg.sv
+ ../../../third_party/basejump_stl/bsg_misc/bsg_counter_up_down.sv
 }
 add_files -fileset constrs_1 -norecurse {
  ../Basys3_Master.xdc
@@ -39,4 +41,27 @@ wait_on_run impl_1
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
+# Open Hardware Manager
+open_hw
+
+# Connect to the FPGA
+connect_hw_server
+open_hw_target
+
+# Set the device (Auto-detect)
+set device [lindex [get_hw_devices] 0]
+refresh_hw_device -update_hw_probes false $device
+
+# Program the FPGA with the generated bitstream
+set bitstream [file normalize "basys3/basys3.runs/impl_1/basys3.bit"]
+set_property PROGRAM.FILE $bitstream $device
+program_hw_devices $device
+
+# Close the hardware session
+close_hw_target
+disconnect_hw_server
+close_hw
+
+
+close_project
 exit
