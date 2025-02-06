e863e7dd8d8acf02b81bc5c1e7c2980f61a40d6e
aklund <aklund@ucsc.edu>
2025-02-06 11:53:08 -0800

    sin and square wave modules

--- rtl/sinusoid_wave.sv
+++ rtl/sinusoid_wave.sv
@@ -17,7 +17,7 @@ module sinusoid
   logic [width_p - 1 : 0] sine_w;
   assign valid_o = 1'b1;
 
-  sinusoid_counter
+  wave_counter
     #(.max_val_p(depth_p - 1))
   addr_counter_inst
     (.clk_i(clk_i)
@@ -44,45 +44,4 @@ module sinusoid
     for (int i = 0; i < depth_p; i++)
       mem[i] = $rtoi((max_val_lp) * $sin(note_freq_p * i * 2 * pi_lp / sampling_freq_p));
   end
-endmodule
-
-
-module sinusoid_counter
-  // this is just a regular counter, named differently to not conflict with
-  // any other counter you  might use
-  #(parameter [31:0] max_val_p = 15
-   ,parameter width_p = $clog2(max_val_p)
-    /* verilator lint_off WIDTHTRUNC */
-   ,parameter [width_p-1:0] reset_val_p = '0
-    )
-    /* verilator lint_on WIDTHTRUNC */
-   (input [0:0] clk_i
-   ,input [0:0] reset_i
-   ,input [0:0] up_i
-   ,input [0:0] down_i
-   ,output logic [width_p-1:0] count_no
-   ,output logic [width_p-1:0] count_o);
-
-  localparam [width_p-1:0] max_val_lp = max_val_p[width_p-1:0];
-
-  always_ff @(posedge clk_i)
-    if (reset_i)
-      count_o <= reset_val_p;
-    else
-      count_o <= count_no;
-
-  always_comb begin
-    count_no = count_o;
-    if (up_i & ~down_i)
-      if (count_o == max_val_lp)
-        count_no = 0;
-      else
-        count_no = count_o + 1;
-    else if (down_i)
-      if (count_o == 0)
-        count_no = max_val_lp;
-      else
-        count_no = count_o - 1;
-  end
-
 endmodule
